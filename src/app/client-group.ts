530ed378018681486bf109994cfecba531cd867f
Artur Morais <arturpmrs@gmail.com>
2023-01-14 18:31:02 -0300

    Criando interfaces para os models

---
+++ src/app/client-group.ts
@@ -0,0 +1,4 @@
+export interface ClientGroup {
+    name: string,
+    active: boolean
+}
---
+++ src/app/client.ts
@@ -0,0 +1,9 @@
+export interface Client {
+    name: string,
+    documentType: string,
+    document: string,
+    rg: string,
+    registrationDate: Date,
+    clientGroup: string,
+    active: boolean
+}
