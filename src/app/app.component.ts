95c8007e729d6083ba79ac6c19b10893199b61b0
Artur Morais <arturpmrs@gmail.com>
2023-01-14 18:33:41 -0300

    Configuração de AppComponent para buscar ClientGroups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ClientGroup } from './client-group';
+import { ClientGroupService } from './client-group.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'clientscrud-client';
+
+  public clientGroups: ClientGroup[] = []
+
+  constructor(private clientGroupService: ClientGroupService) {}
+
+  ngOnInit() {
+    this.getClientGroups()
+  }
+
+  public getClientGroups(): void {
+    this.clientGroupService.getClientGroups().subscribe(
+      (response: ClientGroup[]) => {
+        this.clientGroups = response
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
 }
