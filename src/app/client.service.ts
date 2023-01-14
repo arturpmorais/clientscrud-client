efaa83e584fbccebabde8e4312153302173e199b
Artur Morais <arturpmrs@gmail.com>
2023-01-14 18:32:49 -0300

    Criando services para Client e ClientGroup

---
+++ src/app/client.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Client } from './client';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient){}
+
+  public getClients(): Observable<Client[]> {
+    return this.http.get<Client[]>(`${this.apiServerUrl}/clients`);
+  }
+
+  public addClient(clientGroup: Client): Observable<Client> {
+    return this.http.post<Client>(`${this.apiServerUrl}/clients`, clientGroup);
+  }
+
+  public updateClient(clientGroup: Client): Observable<Client> {
+    return this.http.put<Client>(`${this.apiServerUrl}/clients`, clientGroup);
+  }
+
+  public deleteClient(clientGroupId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/clients/${clientGroupId}`);
+  }
+}
