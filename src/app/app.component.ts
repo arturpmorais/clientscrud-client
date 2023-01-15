ef23ad496c09ba7a9287f144c2ab76f90aecfa01
Artur Morais <arturpmrs@gmail.com>
2023-01-15 14:40:53 -0300

    Label para document1 e document2 dinâmico de acordo com documentType

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,6 +27,29 @@ export class AppComponent implements OnInit {
     this.getClients()
   }
 
+  public labelForDocumentField(field: string, documentType: string): string {
+    switch(field) {
+      case 'document1':
+        switch(documentType) {
+          case 'pf':
+            return 'CPF'
+          case 'pj':
+            return 'CNPJ'
+        }
+        break
+      case 'document2':
+        switch(documentType) {
+          case 'pf':
+            return 'RG'
+          case 'pj':
+            return 'Inscrição Estadual'
+        }
+        break
+    }
+
+    return 'Selecione o Tipo de Cliente'
+  }
+
   public getClientGroups(): void {
     this.clientGroupService.getClientGroups().subscribe({
       next: (response: ClientGroup[]) => {
@@ -47,33 +70,6 @@ export class AppComponent implements OnInit {
         alert(error.message)
       }
     })
-  }
-
-  public onOpenClientGroupModal(clientGroup: ClientGroup | null, mode: string): void {
-    const container = <HTMLElement>document.getElementById('main-container');
-    let modal
-    const button = document.createElement('button');
-
-    button.type = 'button';
-    button.style.display = 'none';
-    button.setAttribute('data-bs-toggle', 'modal');
-
-    switch(mode) {
-      case 'add':
-        button.setAttribute('data-bs-target', '#addClientGroupModal');
-        break
-      case 'edit':
-        this.currentClientGroup = clientGroup;
-        button.setAttribute('data-bs-target', '#updateClientGroupModal');
-        break
-      case 'delete':
-        this.currentClientGroup = clientGroup;
-        button.setAttribute('data-bs-target', '#deleteClientGroupModal');
-        break
-    }
-    
-    container.appendChild(button);
-    button.click();
   }
 
   public onEditClientGroupClick(clientGroup: ClientGroup | null) {
