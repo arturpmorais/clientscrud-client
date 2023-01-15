8c85f86ad10a96a9c3ba5dcd255443c063d007cc
Artur Morais <arturpmrs@gmail.com>
2023-01-15 01:41:11 -0300

    Mudança no schema

--- src/app/interfaces/client.ts
+++ src/app/interfaces/client.ts
@@ -2,8 +2,8 @@ export interface Client {
     id: number,
     name: string,
     documentType: string,
-    document: string,
-    rg: string,
+    document1: string,
+    document2: string,
     registrationDate: Date,
     clientGroup: string,
     active: boolean
