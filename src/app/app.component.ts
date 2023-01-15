1608a07ad7f5a196e3de4720ca995f190edd7a7b
Artur Morais <arturpmrs@gmail.com>
2023-01-15 01:20:14 -0300

    Funções para obter, atualizar e deletar os registros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Client } from './interfaces/client';
 import { ClientGroup } from './interfaces/client-group';
+import { ClientService } from './services/client.service';
 import { ClientGroupService } from './services/client-group.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +15,16 @@ export class AppComponent implements OnInit {
   title = 'clientscrud-client';
 
   public clientGroups: ClientGroup[] = []
+  public currentClientGroup: ClientGroup | null = null
 
-  constructor(private clientGroupService: ClientGroupService) {}
+  public clients: Client[] = []
+  public currentClient: Client | null = null
+
+  constructor(private clientGroupService: ClientGroupService, private clientService: ClientService) {}
 
   ngOnInit() {
     this.getClientGroups()
+    this.getClients()
   }
 
   public getClientGroups(): void {
@@ -28,5 +36,83 @@ export class AppComponent implements OnInit {
         alert(error.message)
       }
     })
+  }
+
+  public getClients(): void {
+    this.clientService.getClients().subscribe({
+      next: (response: Client[]) => {
+        this.clients = response
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    })
+  }
+
+  public onOpenClientGroupModal(clientGroup: ClientGroup | null, mode: string): void {
+    const container = <HTMLElement>document.getElementById('main-container');
+    let modal
+    const button = document.createElement('button');
+
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-bs-toggle', 'modal');
+
+    switch(mode) {
+      case 'add':
+        button.setAttribute('data-bs-target', '#addClientGroupModal');
+        break
+      case 'edit':
+        this.currentClientGroup = clientGroup;
+        button.setAttribute('data-bs-target', '#updateClientGroupModal');
+        break
+      case 'delete':
+        this.currentClientGroup = clientGroup;
+        button.setAttribute('data-bs-target', '#deleteClientGroupModal');
+        break
+    }
+
+    console.log('currentClientGroup:')
+    console.log(this.currentClientGroup)
+    console.log('mode:' + mode)
+    
+    container.appendChild(button);
+    button.click();
+  }
+
+  public onAddClientGroup(addClientGroupForm: NgForm): void {
+    this.clientGroupService.addClientGroup(addClientGroupForm.value).subscribe(
+      (response: ClientGroup) => {
+        this.getClientGroups();
+        addClientGroupForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addClientGroupForm.reset();
+      }
+    );
+  }
+
+  public onUpdateClientGroup(clientGroup: ClientGroup): void {
+    this.clientGroupService.updateClientGroup(clientGroup).subscribe(
+      (response: ClientGroup) => {
+        this.getClientGroups();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteClientGroup(clientGroup: ClientGroup): void {
+    if (confirm('Tem certeza de que deseja deletar este Grupo de Clientes?'))
+      this.clientGroupService.deleteClientGroup(clientGroup.id).subscribe(
+        () => {
+          this.getClientGroups();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      );
   }
 }
